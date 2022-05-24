738430c544c24141e3fda42aecc81e2d956a1468
saltydog1980 <mikeelambert99@gmail.com>
2022-05-24 17:11:31 -0400

    final code

--- js/romanNumerals.js
+++ js/romanNumerals.js
@@ -1,3 +1,11 @@
-exports.toRoman = function(num) {
-
+exports.toRoman = function (num) {
+    const romanKeyMap = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
+    let romanString = '';
+    for (let key in romanKeyMap) {
+        while (num >= romanKeyMap[key]) {
+            romanString += key;
+            num -= romanKeyMap[key];
+        }
+    }
+    return romanString;
 };
