420bb2e7797bd39dc54398dfe4c5c9a66b0d38ac
saltydog1980 <mikeelambert99@gmail.com>
2022-05-25 18:06:09 -0400

    finished python exercise

--- python/roman_numerals.py
+++ python/roman_numerals.py
@@ -1,2 +1,8 @@
 def to_roman(num):
-    # write your code here!
+    roman_keys = { 'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90, 'L': 50, 'XL': 40, 'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1 }
+    roman_string = ""
+    for key in roman_keys:
+        while num >= roman_keys[key]:
+            roman_string += key
+            num -= roman_keys[key]
+    return roman_string
